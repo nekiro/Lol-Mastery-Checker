ba9c130b1278575b132df2b2f0b8b100421615e1
Nekiro <nekiro.dev@gmail.com>
2022-08-02 13:08:42 +0200

    Reformat files

--- js/podium.js
+++ js/podium.js
@@ -2,66 +2,72 @@
 const { ipcRenderer } = require('electron');
 
 // exit button
-const exitBtn = document.querySelector(".close-btn")
-exitBtn.addEventListener("click", () => { ipcRenderer.send('menuBtnAction', "podium-exit") })
+const exitBtn = document.querySelector('.close-btn');
+exitBtn.addEventListener('click', () => {
+  ipcRenderer.send('menuBtnAction', 'podium-exit');
+});
 
 // minimize
-const minimizeBtn = document.querySelector(".minimize-btn")
-minimizeBtn.addEventListener("click", () => { ipcRenderer.send('menuBtnAction', "podium-minimize") })
+const minimizeBtn = document.querySelector('.minimize-btn');
+minimizeBtn.addEventListener('click', () => {
+  ipcRenderer.send('menuBtnAction', 'podium-minimize');
+});
 
 // open main window
-const openMainBtn = document.querySelector(".open-main-btn")
-openMainBtn.addEventListener("click", () => { ipcRenderer.send('menuBtnAction', "open-main") })
+const openMainBtn = document.querySelector('.open-main-btn');
+openMainBtn.addEventListener('click', () => {
+  ipcRenderer.send('menuBtnAction', 'open-main');
+});
 
 // podium list
-const podiumList = document.querySelector(".podium-list")
+const podiumList = document.querySelector('.podium-list');
 
-var shouldUpdatePodium = true
+var shouldUpdatePodium = true;
 
 function updatePodium() {
-    if (!shouldUpdatePodium) {
-        return
-    }
+  if (!shouldUpdatePodium) {
+    return;
+  }
 
-    ipcRenderer.send("updatePodium")
+  ipcRenderer.send('updatePodium');
 
-    // loop the update
-    setTimeout(updatePodium, 60000)
+  // loop the update
+  setTimeout(updatePodium, 60000);
 }
 
 // fire first update
-updatePodium()
+updatePodium();
 
-ipcRenderer.on("updatePodium", async (event, args) => {
-    if (!args.success) {
-        shouldUpdatePodium = false
-        return;
-    }
+ipcRenderer.on('updatePodium', async (event, args) => {
+  if (!args.success) {
+    shouldUpdatePodium = false;
+    return;
+  }
 
-    podiumList.innerHTML = ""
+  podiumList.innerHTML = '';
 
-    args.podium.forEach(element => {
-        let champImg = document.createElement("img")
-        champImg.alt = "champ"
-        champImg.src = element.imgUrl
-        champImg.style.pointerEvents = "none"
+  args.podium.forEach((element) => {
+    let champImg = document.createElement('img');
+    champImg.alt = 'champ';
+    champImg.src = element.imgUrl;
+    champImg.style.pointerEvents = 'none';
 
-        podiumList.appendChild(champImg)
+    podiumList.appendChild(champImg);
 
-        let div = document.createElement("div")
+    let div = document.createElement('div');
 
-        let elem = document.createElement("div")
-        elem.id = "champMasteryPts"
-        elem.innerText = `${element.championPoints} points`
+    let elem = document.createElement('div');
+    elem.id = 'champMasteryPts';
+    elem.innerText = `${element.championPoints} points`;
 
-        div.appendChild(elem)
+    div.appendChild(elem);
 
-        elem = document.createElement("div")
-        elem.id = "champMasteryNextLvl"
-        elem.innerText = `${element.championPointsUntilNextLevel} to go`
+    elem = document.createElement('div');
+    elem.id = 'champMasteryNextLvl';
+    elem.innerText = `${element.championPointsUntilNextLevel} to go`;
 
-        div.appendChild(elem)
+    div.appendChild(elem);
 
-        podiumList.appendChild(div)
-    })
-})
+    podiumList.appendChild(div);
+  });
+});
--- main.js
+++ main.js
@@ -1,40 +1,40 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron');
 
-require("./js/ipcMain.js")
+require('./js/ipcMain.js');
 
-exports.createMainWindow = function() {
-    const mainWindow = new BrowserWindow({
-		width: 800,
-		height: 600,
-		minHeight: 600,
-		maxHeight: 1200,
-		minWidth: 800,
-		maxWidth: 800,
-		frame: false,
-		webPreferences: {
-			nodeIntegration: true,
-			contextIsolation: false,
-		},
-		title: "Mastery Checker"
-	})
+exports.createMainWindow = function () {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    minHeight: 600,
+    maxHeight: 1200,
+    minWidth: 800,
+    maxWidth: 800,
+    frame: false,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+    title: 'Mastery Checker',
+  });
 
-	mainWindow.loadFile('./html/index.html')
+  mainWindow.loadFile('./html/index.html');
 
-	// Open the DevTools.
-	//mainWindow.webContents.openDevTools()
-    return mainWindow
-}
+  // Open the DevTools.
+  //mainWindow.webContents.openDevTools()
+  return mainWindow;
+};
 
 app.whenReady().then(() => {
-	exports.createMainWindow()
+  exports.createMainWindow();
 
-	app.on('activate', function () {
-		// On macOS it's common to re-create a window in the app when the
-		// dock icon is clicked and there are no other windows open.
-		if (BrowserWindow.getAllWindows().length === 0) createWindow()
-	})
-})
+  app.on('activate', function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
 
 app.on('window-all-closed', function () {
-	if (process.platform !== 'darwin') app.quit()
-})
+  if (process.platform !== 'darwin') app.quit();
+});
